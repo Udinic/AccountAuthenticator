cbdb9b970f6b91ad2702750ebbaa34ce21b35405
JP Ventura <contact@jpventura.com>
2017-05-20 09:35:30 -0300

    :books: Apache License v2.0

--- accountAuthenticator/src/main/java/com/udinic/accounts_authenticator_example/authentication/ServerAuthenticate.java
+++ accountAuthenticator/src/main/java/com/udinic/accounts_authenticator_example/authentication/ServerAuthenticate.java
@@ -1,10 +1,21 @@
+/*
+ * Copyright (C) 2017 Udi Cohen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.udinic.accounts_authenticator_example.authentication;
 
-/**
- * User: udinic
- * Date: 3/27/13
- * Time: 2:35 AM
- */
 public interface ServerAuthenticate {
     public String userSignUp(final String name, final String email, final String pass, String authType) throws Exception;
 
--- accountAuthenticator/src/main/java/com/udinic/accounts_authenticator_example/authentication/UdinicAuthenticator.java
+++ accountAuthenticator/src/main/java/com/udinic/accounts_authenticator_example/authentication/UdinicAuthenticator.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2017 Udi Cohen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.udinic.accounts_authenticator_example.authentication;
 
 import android.accounts.*;
@@ -10,12 +26,6 @@ import android.util.Log;
 import static android.accounts.AccountManager.KEY_BOOLEAN_RESULT;
 import static com.udinic.accounts_authenticator_example.authentication.AccountGeneral.*;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Udini
- * Date: 19/03/13
- * Time: 18:58
- */
 public class UdinicAuthenticator extends AbstractAccountAuthenticator {
 
     private String TAG = "UdinicAuthenticator";
